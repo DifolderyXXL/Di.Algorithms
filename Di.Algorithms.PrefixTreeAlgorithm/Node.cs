ab07304469d02dc1c231a54af2c4f06b64085ce2
DifolderyXXL <87066000+DifolderyXXL@users.noreply.github.com>
2023-06-15 15:57:48 +0300

    Initial commit

---
+++ Di.Algorithms.PrefixTreeAlgorithm/Node.cs
@@ -0,0 +1,23 @@
+﻿namespace Di.Algorithms.PrefixTreeAlgorithm;
+
+public class Node<T> 
+{
+    public char Symbol { get; set; }
+
+    public bool IsWord { get; set; }
+    
+    public T Data { get; set; }
+
+    public Dictionary<char, Node<T>> SubNodes { get; set; } = new();
+    
+    public Node(char symbol, T data)
+    {
+        Symbol = symbol;
+        Data = data;
+    }
+
+    public bool TryFind(char symbol, out Node<T> node)
+    {
+        return SubNodes.TryGetValue(symbol, out node);
+    }
+}
---
+++ Di.Algorithms.PrefixTreeAlgorithm/PrefixTree.cs
@@ -0,0 +1,30 @@
+﻿namespace Di.Algorithms.PrefixTreeAlgorithm;
+
+public class PrefixTree<T>
+{
+    private Node<T> _root;
+
+    public void Add(string key, T data)
+    {
+        
+    }
+
+    private void AddNode(string key, T data, Node<T> node)
+    {
+        node.IsWord = node.SubNodes.Count == 0;
+        
+        if (key.Length == 0)
+            return;
+        
+        if (node.TryFind(key[0], out var curSubNode))
+        {
+            AddNode(key.Remove(0), data, curSubNode);
+        }
+        else
+        {
+            var subNode = new Node<T>(key[0], data);
+            node.SubNodes.Add(key[0], subNode);
+            AddNode(key.Remove(0), data, subNode);
+        }
+    }
+}
